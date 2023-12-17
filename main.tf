bbdfadaab05289a46c341e6b81a0e2d49942f34c
Christian S <85380398+steve594@users.noreply.github.com>
2023-12-16 22:55:27 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+  name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t3.nano"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
